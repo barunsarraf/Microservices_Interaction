6eb70c6d780a39195f69b0d2edb1f85d1b54fd3b
Barun Sarraf <barun.sarraf@cgi.com>
2019-10-12 12:00:43 +0530

    working with h2 and mongo and docker

---
+++ MultiModel/src/test/java/com/stackroute/MultiModel/MultiModelApplicationTests.java
@@ -0,0 +1,16 @@
+package com.stackroute.MultiModel;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MultiModelApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
